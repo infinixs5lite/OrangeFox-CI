5420e5daf1f2457330a11ecf7d518e16ab9f1363
infinixs5lite <kelvinchinedu97@gmail.com>
2022-07-18 07:29:59 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_11"
+export DT_LINK=https://gitlab.com/OrangeFox/Manifest.git
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE=X688B
+export OEM=Infinix
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
