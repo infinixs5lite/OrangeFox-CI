9aeab678f0e799b46c48558590e48227c67a5a1f
infinixs5lite <kelvinchinedu97@gmail.com>
2022-12-27 19:28:49 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_10.0"
+export DT_LINK="https://github/infinixs5lite/twrp_device_tecno_KG7h"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="KG7h"
+export OEM="tecno"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
 ## "bootimage" - for A/B devices without recovery partition (and without vendor boot)
 ## "vendorbootimage" - for devices Using vendor boot for the recovery ramdisk (Usually for devices shipped with Android 12 or higher)
-export TARGET="recoveryimage"
+export TARGET="bootimage"
 
 export OUTPUT="OrangeFox*.zip"
 
 # Additional Dependencies (eg: Kernel Source)
 # Format: "repo dest"
 DEPS=(
-    "https://github.com/OrangeFoxRecovery/Avatar.git misc"
+    "https://github.com/infinixs5lite/twrp_device_tecno_KG7h/kernel/tecno/KG7h
 )
 
 # Extra Command
